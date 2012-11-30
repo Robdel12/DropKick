16eaa3e04a91aac50d6507fc0b0c3683d8c3b260
Steve Vanspall <steve.vanspall@jedo.me>
2012-11-30 11:52:35 +1100

    stevev-v-jedo: Fixed browser detection to get dropkick working in ie10

--- jquery.dropkick-1.0.0.js
+++ jquery.dropkick-1.0.0.js
@@ -13,7 +13,7 @@
   var ie6 = false;
 
   // Help prevent flashes of unstyled content
-  if ($.browser.msie && $.browser.version.substr(0, 1) < 7) {
+  if ($.browser.msie && $.browser.version.match(/^(\d+)\./)[1] < 7) {
     ie6 = true;
   } else {
     document.documentElement.className = document.documentElement.className + ' dk_fouc';
