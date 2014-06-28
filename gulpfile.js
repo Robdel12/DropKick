6ad2fcd4b2b9e32c1fd287bec74ab37142e0331a
Robert DeLuca <robertd@izea.com>
2014-06-28 17:46:57 -0400

    Added things to the gulp file for js linting, sass compling, and js minifiying

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,45 @@
 var gulp = require('gulp'),
-    qunit = require('gulp-qunit');
+    qunit = require('gulp-qunit'),
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
 
 gulp.task('default', function() {
-    return gulp.src('./tests/src/runner.html')
-        .pipe(qunit());
+  return gulp.src('./tests/src/runner.html')
+    .pipe(qunit());
+});
+
+// Lint Task
+gulp.task('lint', function() {
+  return gulp.src('dropkick.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+  return gulp.src('css/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+  return gulp.src('dropkick.js')
+    .pipe(rename('dropkick.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(''));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+  gulp.watch('dropkick.js', ['lint', 'scripts']);
+  gulp.watch('css/*.scss', ['sass']);
 });
 
 gulp.task('test', function() {
-    return gulp.src('./tests/src/runner.html')
-        .pipe(qunit());
+  return gulp.src('./tests/src/runner.html')
+    .pipe(qunit());
 });
