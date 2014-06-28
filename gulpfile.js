7f4e6f5e803937b060d61a315af9d1bee7b9fdad
Robert DeLuca <robertd@izea.com>
2014-06-28 11:48:20 -0400

    Unbreaking Travis Ci

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp'),
     qunit = require('gulp-qunit');
 
+gulp.task('default', function() {
+    return gulp.src('./tests/src/runner.html')
+        .pipe(qunit());
+});
+
 gulp.task('test', function() {
     return gulp.src('./tests/src/runner.html')
         .pipe(qunit());
