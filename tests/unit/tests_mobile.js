3324435ac6c1df8eb749fed70d400b0694b36f4b
Robert DeLuca <robertdeluca19@gmail.com>
2016-04-24 01:28:39 -0500

    Add tests for #310. Fixes #311 (#333)

--- tests/unit/tests_mobile.js
+++ tests/unit/tests_mobile.js
@@ -7,6 +7,16 @@ QUnit.test( "Dropkick methods should still work when not rendered on mobile", 3,
   assert.notEqual($sel.attr("data-dkCacheId"), dk.data.cacheID + '');
 
   dk.select(2); //2 = Alaska
+  dk.refresh();
 
   assert.equal($sel.val(), dk.value);
+});
+
+QUnit.test( "Dropkick shouldn't blow up when calling methods on mobile: false dks", 1, function( assert ) {
+  var $sel = $("#normal_select", {mobile: false}),
+      dk = new Dropkick($sel[0]);
+
+  assert.equal(navigator.userAgent, "Android");
+
+  dk.refresh();
 });
